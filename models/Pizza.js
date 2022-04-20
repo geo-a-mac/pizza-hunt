f21ba16dcd8b0d9617c448ed24a22eaccb26294e
George Mcalpine <georgemcalpine@Georges-iMac.local>
2022-04-20 11:57:16 -0400

    pizza model, controller, routes and front end

---
+++ models/Pizza.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large' 
+    },
+    toppings: []
+})
+
+// create the model using the schema
+const Pizza = model('Pizza', PizzaSchema);
+// export the model
+module.exports = Pizza;
